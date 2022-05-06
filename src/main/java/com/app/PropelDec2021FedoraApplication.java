d3804c26805742913afcd339773b17980bfe3d08
NMN27 <nirmalkalamurali@gmail.com>
2022-05-06 15:50:40 +0530

    First Commit

---
+++ src/main/java/com/app/PropelDec2021FedoraApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropelDec2021FedoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropelDec2021FedoraApplication.class, args);
+	}
+
+}
